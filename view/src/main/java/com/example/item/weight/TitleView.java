c03ee9805f7a720f74f47aad03c1398a107371fd
HuXiaobai <1203747102@qq.com>
2018-10-26 10:52:34 +0800

    第一次构建项目

--- view/src/main/java/com/example/item/weight/TitleView.java
+++ view/src/main/java/com/example/item/weight/TitleView.java
@@ -2,6 +2,7 @@ package com.example.item.weight;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.util.TypedValue;
@@ -21,6 +22,24 @@ public class TitleView extends RelativeLayout {
     public TextView mTvSure;
     public ViewGroup mViewRoot;
 
+    public void setOnBackViewClickListener(OnBackViewClickListener onBackViewClickListener) {
+        this.onBackViewClickListener = onBackViewClickListener;
+    }
+
+    private OnBackViewClickListener onBackViewClickListener;
+
+    public void setOnTitleViewClickListener(OnTitleViewClickListener onTitleViewClickListener) {
+        this.onTitleViewClickListener = onTitleViewClickListener;
+    }
+
+    private OnTitleViewClickListener onTitleViewClickListener;
+
+    public void setOnSureViewClickListener(OnSureViewClickListener onSureViewClickListener) {
+        this.onSureViewClickListener = onSureViewClickListener;
+    }
+
+    private OnSureViewClickListener onSureViewClickListener;
+
     public TitleView(Context context) {
         this(context, null);
     }
@@ -29,8 +48,10 @@ public class TitleView extends RelativeLayout {
         super(context, attrs);
         initView();
         initAttrs(attrs);
+        initEvent();
 
     }
+
 
     private void initView() {
         View inflate = View.inflate(getContext(), R.layout.view_title, this);
@@ -48,25 +69,52 @@ public class TitleView extends RelativeLayout {
             mTvBack.setCompoundDrawablesWithIntrinsicBounds(typedArray.getDrawable(R.styleable.TitleView_back_left_drawable), typedArray.getDrawable(R.styleable.TitleView_back_top_drawable),
                     typedArray.getDrawable(R.styleable.TitleView_back_right_drawable), typedArray.getDrawable(R.styleable.TitleView_back_bottom_drawable));
             mTvBack.setCompoundDrawablePadding(R.styleable.TitleView_back_drawable_padding);
-            mTvBack.setPadding(typedArray.getDimensionPixelSize(R.styleable.TitleView_back_text_padding_left,0),typedArray.getDimensionPixelSize(R.styleable.TitleView_back_text_padding_top,0),
-                    typedArray.getDimensionPixelSize(R.styleable.TitleView_back_text_padding_right,0),typedArray.getDimensionPixelSize(R.styleable.TitleView_back_text_padding_bottom,0));
-            mTvBack.setTextSize(TypedValue.COMPLEX_UNIT_PX,typedArray.getDimensionPixelSize(R.styleable.TitleView_back_text_size, (int) mTvBack.getTextSize()));
+            mTvBack.setPadding(typedArray.getDimensionPixelSize(R.styleable.TitleView_back_text_padding_left, 0), typedArray.getDimensionPixelSize(R.styleable.TitleView_back_text_padding_top, 0),
+                    typedArray.getDimensionPixelSize(R.styleable.TitleView_back_text_padding_right, 0), typedArray.getDimensionPixelSize(R.styleable.TitleView_back_text_padding_bottom, 0));
+            mTvBack.setTextSize(TypedValue.COMPLEX_UNIT_PX, typedArray.getDimensionPixelSize(R.styleable.TitleView_back_text_size, (int) mTvBack.getTextSize()));
             mTvBack.setTextColor(typedArray.getColor(R.styleable.TitleView_back_text_color, ContextCompat.getColor(getContext(), R.color.colorFFEFEFEF)));
             mTvCenter.setText(typedArray.getString(R.styleable.TitleView_center_data));
             mTvCenter.setTextColor(typedArray.getColor(R.styleable.TitleView_center_text_color, ContextCompat.getColor(getContext(), R.color.colorFFFFFFFF)));
-            mTvCenter.setTextSize(TypedValue.COMPLEX_UNIT_PX,typedArray.getDimensionPixelSize(R.styleable.TitleView_center_text_size, (int) mTvCenter.getTextSize()));
+            mTvCenter.setTextSize(TypedValue.COMPLEX_UNIT_PX, typedArray.getDimensionPixelSize(R.styleable.TitleView_center_text_size, (int) mTvCenter.getTextSize()));
             mTvSure.setText(typedArray.getString(R.styleable.TitleView_center_data));
             mTvSure.setCompoundDrawablesWithIntrinsicBounds(typedArray.getDrawable(R.styleable.TitleView_sure_left_drawable), typedArray.getDrawable(R.styleable.TitleView_sure_top_drawable),
                     typedArray.getDrawable(R.styleable.TitleView_sure_right_drawable), typedArray.getDrawable(R.styleable.TitleView_sure_bottom_drawable));
-            mTvSure.setTextSize(TypedValue.COMPLEX_UNIT_PX,typedArray.getDimensionPixelSize(R.styleable.TitleView_sure_text_size, (int) mTvSure.getTextSize()));
+            mTvSure.setTextSize(TypedValue.COMPLEX_UNIT_PX, typedArray.getDimensionPixelSize(R.styleable.TitleView_sure_text_size, (int) mTvSure.getTextSize()));
             mTvSure.setTextColor(typedArray.getColor(R.styleable.TitleView_sure_text_color, ContextCompat.getColor(getContext(), R.color.colorFFE1E1E1)));
             mTvSure.setCompoundDrawablePadding(R.styleable.TitleView_sure_drawable_padding);
-            mTvSure.setPadding(typedArray.getDimensionPixelSize(R.styleable.TitleView_sure_text_padding_left,0),typedArray.getDimensionPixelSize(R.styleable.TitleView_sure_text_padding_top,0),
-                    typedArray.getDimensionPixelSize(R.styleable.TitleView_sure_text_padding_right,0),typedArray.getDimensionPixelSize(R.styleable.TitleView_sure_text_padding_bottom,0));
+            mTvSure.setPadding(typedArray.getDimensionPixelSize(R.styleable.TitleView_sure_text_padding_left, 0), typedArray.getDimensionPixelSize(R.styleable.TitleView_sure_text_padding_top, 0),
+                    typedArray.getDimensionPixelSize(R.styleable.TitleView_sure_text_padding_right, 0), typedArray.getDimensionPixelSize(R.styleable.TitleView_sure_text_padding_bottom, 0));
             typedArray.recycle();
         }
 
+
     }
+
+    private void initEvent() {
+        mTvBack.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (onTitleViewClickListener != null) {
+                    onTitleViewClickListener.onBackClick(v);
+                }
+                if (onBackViewClickListener != null) {
+                    onBackViewClickListener.onBackClick(v);
+                }
+            }
+        });
+        mTvSure.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (onTitleViewClickListener != null) {
+                    onTitleViewClickListener.onSureClick(v);
+                }
+                if (onSureViewClickListener != null) {
+                    onSureViewClickListener.onSureClick(v);
+                }
+            }
+        });
+    }
+
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -76,10 +124,10 @@ public class TitleView extends RelativeLayout {
         int resultHeightSize = ScreenUtils.dp2px(getContext(), Contast.DEFAULT_VIEW_HEIGHT);
         switch (heightMeasureMode) {
             case MeasureSpec.UNSPECIFIED:
-                ScreenUtils.setDefaultRootViewSize(getContext(),mViewRoot);
+                ScreenUtils.setDefaultRootViewSize(getContext(), mViewRoot);
                 break;
             case MeasureSpec.AT_MOST:
-                ScreenUtils.setDefaultRootViewSize(getContext(),mViewRoot);
+                ScreenUtils.setDefaultRootViewSize(getContext(), mViewRoot);
                 break;
             case MeasureSpec.EXACTLY:
                 resultHeightSize = heightMeasureSize;
@@ -90,4 +138,17 @@ public class TitleView extends RelativeLayout {
         setMeasuredDimension(widthMeasureSpec, resultHeightSize);
     }
 
+    public interface OnTitleViewClickListener {
+        void onBackClick(@NonNull View view);
+
+        void onSureClick(@NonNull View view);
+    }
+
+    public interface OnBackViewClickListener {
+        void onBackClick(@NonNull View view);
+    }
+
+    public interface OnSureViewClickListener {
+        void onSureClick(@NonNull View view);
+    }
 }
